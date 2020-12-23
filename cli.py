4144f22a08414af3396c9bacc79c1ff1c5644783
Justis Gipson <justis710@gmail.com>
2020-12-23 18:37:24 -0500

    add base.py, cli.py, data.py

--- cli.py
+++ cli.py
@@ -1,6 +1,8 @@
 import argparse
 import os
+import sys
 
+from . import base
 from . import data
 
 def main ():
@@ -16,8 +18,24 @@ def parse_args():
   init_parser = commands.add_parser('init')
   init_parser.set_defaults(func=init)
 
+  hash_object_parser = commands.add_parser('hash_object')
+  hash_object_parser.set_defaults(func=hash_object)
+  hash_object_parser.add_argument('file')
+
+  cat_file_parser = commands.add_parser('cat-file')
+  cat_file_parser.set_defaults(func=cat_file)
+  cat_file_parser.add_argument('object')
+
   return parser.parse_args()
 
 def init(args):
   data.init()
   print(f'Initialized empty git repository in {os.getcwd()}/{data.GIT_DIR}')
+
+def hash_object(args):
+  with open(args.file, 'rb') as f:
+    print(data.hash_object(f.read()))
+
+def cat_file(args):
+  sys.stdout.flush()
+  sys.stdout.buffer.write(data.get_object(args.object, expected=None))
